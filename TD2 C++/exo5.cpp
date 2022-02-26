843b6f820a6708c3fe6248852fab16f1ca503a61
Alexis Brunet <albrunet2000@lapinfo.fr>
2022-02-26 18:32:39 +0000

    Upload New File

---
+++ TD2 C++/exo5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int tab[9],i;
+    for(i=0;i<9;i++){
+        cout << "Entrer une valeur :";
+        cin >> tab[i];
+    }
+    return 0;
+}
