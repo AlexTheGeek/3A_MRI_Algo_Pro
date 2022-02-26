dfb210dcf64cf5490f36cc341e34830d4875e5be
Alexis Brunet <albrunet2000@lapinfo.fr>
2022-02-26 18:29:25 +0000

    Upload New File

---
+++ TD1 C++/exo2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int nb, carr;
+    cout << "Entrer un nombre : " << endl;
+    cin >> nb;
+    carr = nb*nb;
+    cout << "Son carrré est : "<< carr << endl;
+    return 0;
+}
