1842242ad2ec2c5632b39e4740afe16249d0c75b
Alexis Brunet <albrunet2000@lapinfo.fr>
2022-02-26 18:29:19 +0000

    Upload New File

---
+++ TD1 C++/exo1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    int val = 231, Double;
+    cout << "La valeur de val : "<< val << endl;
+    cout << "La valeur de Double :" << Double << endl;
+    return 0;
+}
